7e7a38dd4bb85b976e4897368bf186289f65aeee
Tomo-Shortty <tomo_007@live.com.au>
2022-02-14 22:11:47 +1000

    Added enums

---
+++ 40KDuels/40KDuels/Enums/FactionKeyword.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _40KDuels.Enums
+{
+    internal enum FactionKeyword
+    {
+        Imperium,
+        Chaos,
+        Aeldari,
+        Orks,
+        Necrons,
+        TauEmpire,
+        Tyranids,
+        Khorne,
+        Tzeentch,
+        Nurgle,
+        Slaanesh
+    }
+}
---
+++ 40KDuels/40KDuels/Enums/TurnPhase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _40KDuels.Enums
+{
+    internal enum TurnPhase
+    {
+        CommandPhase,
+        MovementPhase,
+        PsychicPhase,
+        ShootingPhase,
+        ChargePhase,
+        FightPhase,
+        MoralePhase
+    }
+}
